e9b22a66e2b47c3f7484c2b2c54753d8cc445056
dbortoluzzi <danieleb88@gmail.com>
2021-08-24 11:07:09 +0200

    db embedded

--- backend/consumer-service/src/main/java/eu/dbortoluzzi/consumer/AtmDataSeeder.java
+++ backend/consumer-service/src/main/java/eu/dbortoluzzi/consumer/AtmDataSeeder.java
@@ -7,6 +7,7 @@ import eu.dbortoluzzi.consumer.model.AtmIndexable;
 import eu.dbortoluzzi.consumer.repository.AtmsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,7 @@ public class AtmDataSeeder {
 
 	Logger logger = LoggerFactory.getLogger(AtmDataSeeder.class);
 
+	@Autowired
 	private AtmsRepository atmsRepository;
 
 	private final String atmsUrl;
