38a43f596a1ea09bc9a5c4d4817a0f384ad40160
dbortoluzzi <danieleb88@gmail.com>
2021-08-24 11:55:08 +0200

    swagger

--- backend/consumer-service/src/main/java/eu/dbortoluzzi/consumer/AtmServiceApplication.java
+++ backend/consumer-service/src/main/java/eu/dbortoluzzi/consumer/AtmServiceApplication.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AtmServiceApplication {
 
 	@Autowired
